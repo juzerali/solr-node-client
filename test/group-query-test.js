35f66ac0097327fa93c53c1344434e94f47db16b
Juzer Ali <er.juzerali@gmail.com>
2012-11-17 20:05:29 +0530

    Created a test for simple groupBy query

--- test/group-query-test.js
+++ test/group-query-test.js
@@ -1,1 +1,33 @@
 //TODO support http://wiki.apache.org/solr/FieldCollapsing?highlight=%28field%29%7C%28collapsing%29
+/**
+ * Modules dependencies
+ */
+
+var mocha = require('mocha'),
+	assert = require('chai').assert,
+	libPath = process.env['SOLR_CLIENT_COV'] ? '../lib-cov' : '../lib',
+	solr = require( libPath + '/solr'),
+	sassert = require('./sassert');
+
+// Test suite
+var client = solr.createClient();
+
+
+describe('Group',function(){
+
+	describe('#groupBy(field), callback)',function(){
+		it('should return grouped results',function(done){
+			var query = client.createQuery()
+				.q({
+					title_t: 'title1'
+				})
+				.groupBy('title_t');
+			client.search(query, function(err, data){
+				sassert.ok(err, data);
+				assert(data.responseHeader.params.group);
+				assert.equal('title_t', data.responseHeader.params['group.field']);
+				done();
+			});
+		});
+	});
+});
